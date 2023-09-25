6da3b5c933ea8261c87bc5a7f727be41ebb34e87
Joaquim Gouveia <joaquimgouveia2000@gmail.com>
2023-09-25 16:55:04 +0200

    Created thread pool

--- crack/src/CodeBreaker.java
+++ crack/src/CodeBreaker.java
@@ -1,4 +1,6 @@
 import java.math.BigInteger;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -10,6 +12,8 @@ import client.view.StatusWindow;
 import client.view.WorklistItem;
 import network.Sniffer;
 import network.SnifferCallback;
+import rsa.Factorizer;
+import rsa.ProgressTracker;
 
 public class CodeBreaker implements SnifferCallback {
 
@@ -50,6 +54,7 @@ public class CodeBreaker implements SnifferCallback {
     /** Called by a Sniffer thread when an encrypted message is obtained. */
     @Override
     public void onMessageIntercepted(String message, BigInteger n) {
+        ExecutorService pool = Executors.newFixedThreadPool(2);
         SwingUtilities.invokeLater(() -> {
             WorklistItem workItem = new WorklistItem(n, message);
             JButton breakButton = new JButton("Break");
@@ -57,9 +62,36 @@ public class CodeBreaker implements SnifferCallback {
                 ProgressItem progressItem = new ProgressItem(n, message);
                 progressList.add(progressItem);
                 workList.remove(workItem);
+                ProgressTracker tracker = new Tracker();
+                Runnable task = () -> {
+                    try {
+                        Factorizer.crack(message,n,tracker);
+                    } catch (InterruptedException e1) {
+                        e1.printStackTrace();
+                    }
+                };
+                pool.submit(task);
             });
             workItem.add(breakButton);
             workList.add(workItem);
         });
+    }
+
+    /** ProgressTracker: reports how far factorization has progressed */ 
+    private static class Tracker implements ProgressTracker {
+        private int totalProgress = 0;
+
+        /**
+         * Called by Factorizer to indicate progress. The total sum of
+         * ppmDelta from all calls will add upp to 1000000 (one million).
+         * 
+         * @param  ppmDelta   portion of work done since last call,
+         *                    measured in ppm (parts per million)
+         */
+        @Override
+        public void onProgress(int ppmDelta) {
+            totalProgress += ppmDelta;
+            System.out.println("progress = " + totalProgress + "/1000000");
+        }
     }
 }
